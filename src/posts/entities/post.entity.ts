46d4ebe2a1e300d3ca8781bf0e6af8f291201bb0
Mbonu Basil <mbonubasil@gmail.com>
2023-02-16 13:30:48 +0100

    Setup posts entity and add it the db

--- src/posts/entities/post.entity.ts
+++ src/posts/entities/post.entity.ts
@@ -1,6 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('posts')
 export class Post {
+    @PrimaryGeneratedColumn()
     id: number;
+
+    @Column()
     title: string;
+
+    @Column()
     body: string;
+
+    @Column()
     userId: string;
 }
