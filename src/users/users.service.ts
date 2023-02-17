06baec57b733840492d62feae2d77c15a186fbe3
Mbonu Basil <mbonubasil@gmail.com>
2023-02-17 13:06:55 +0100

    Check for existing user before creating another

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,6 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { hashPassword } from 'src/utils';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -12,8 +13,15 @@ export class UsersService {
     private usersRepository: Repository<User>
   ) { }
 
-  create(createUserDto: CreateUserDto) {
-    const user = this.usersRepository.create(createUserDto);
+  async create(createUserDto: CreateUserDto) {
+    const userExist = this.findByEmail(createUserDto.email);
+    if (userExist) {
+      throw new BadRequestException(`User with email ${createUserDto.email} already exists`)
+    }
+    //encrypted password
+    const password: string = await hashPassword(createUserDto.password);
+    const userData = { ...createUserDto, password }
+    const user = this.usersRepository.create(userData);
     return this.usersRepository.save(user);
   }
 
@@ -30,7 +38,7 @@ export class UsersService {
   }
 
   async findByEmail(email: string): Promise<User | undefined> {
-    return this.usersRepository.findOneBy({ email }) //this.users.find(user => user.username === username);
+    return this.usersRepository.findOneBy({ email });
   }
 
   async update(id: string, updateUserDto: UpdateUserDto) {
