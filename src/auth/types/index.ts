fa67e88e0eac0cd7be6b537416083ff79eef9f53
Mbonu Basil <mbonubasil@gmail.com>
2023-02-17 17:58:37 +0100

    Add JwtPayload in types

---
+++ src/auth/types/index.ts
@@ -0,0 +1,11 @@
+export type Tokens = {
+    access_token: string;
+    refresh_token: string;
+}
+
+export type JwtPayload = {
+    sub: string;
+    email: string;
+    iat: number;
+    exp: number;
+}
