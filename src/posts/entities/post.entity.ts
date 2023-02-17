f91538f3bc0b9aaa04c8bd44a8ca86047dea3ac7
Mbonu Basil <mbonubasil@gmail.com>
2023-02-17 13:16:41 +0100

    Add relations to both entities

--- src/posts/entities/post.entity.ts
+++ src/posts/entities/post.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { User } from "src/users/entities/user.entity";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity('posts')
 export class Post {
@@ -11,6 +12,6 @@ export class Post {
     @Column()
     body: string;
 
-    @Column()
-    userId: number;
+    @ManyToOne(() => User, (author) => author.posts)
+    author: User;
 }
