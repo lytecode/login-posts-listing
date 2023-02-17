d7108a8785a9118c57dc3c42d7bd111652eb79b9
Mbonu Basil <mbonubasil@gmail.com>
2023-02-17 20:08:20 +0100

    Decorate public routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,8 @@
-import { Body, Controller, HttpCode, HttpStatus, Post, Req, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { Request } from 'express';
 import { AuthService } from './auth.service';
+import { GetCurrentUser, GetCurrentUserId, Public } from './common/decorators';
+import { RefreshTokenGuard } from './common/guards/rt.guards';
 import { AuthDto } from './dto/auth.dto';
 import { Tokens } from './types';
 
@@ -11,31 +11,34 @@ import { Tokens } from './types';
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
 
+  @Public()
   @Post('/signup')
   @HttpCode(HttpStatus.CREATED)
   signup(@Body() authDto: AuthDto): Promise<Tokens> {
     return this.authService.signup(authDto);
   }
 
+  @Public()
   @Post('/login')
   @HttpCode(HttpStatus.OK)
   login(@Body() authDto: AuthDto): Promise<Tokens> {
     return this.authService.login(authDto);
   }
 
-  @UseGuards(AuthGuard('jwt'))
   @Post('/logout')
   @HttpCode(HttpStatus.OK)
-  logout(@Req() req: Request) {
-    const user: any = req.user
-    return this.authService.logout(user.sub);
+  logout(@GetCurrentUserId() userId: string) {
+    return this.authService.logout(userId);
   }
 
-  @UseGuards(AuthGuard('jwt-refresh'))
+  @Public()
+  @UseGuards(RefreshTokenGuard)
   @Post('/refresh')
   @HttpCode(HttpStatus.OK)
-  refreshToken(@Req() req: Request) {
-    const user: any = req.user
-    return this.authService.refreshToken(user.sub, user.refreshToken);
+  refreshToken(
+    @GetCurrentUserId() userId: string,
+    @GetCurrentUser('refreshToken') refreshToken: string
+  ) {
+    return this.authService.refreshToken(userId, refreshToken);
   }
 }
