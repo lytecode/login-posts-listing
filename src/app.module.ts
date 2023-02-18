4fa7248fbccaac387fa7fc21da21aac38fe2d011
Mbonu Basil <mbonubasil@gmail.com>
2023-02-18 20:49:08 +0100

    Remove user module from app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostsModule } from './posts/posts.module';
-import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { AccessTokenGuard } from './auth/common/guards/at.gurads';
@@ -9,7 +8,6 @@ import { AccessTokenGuard } from './auth/common/guards/at.gurads';
 @Module({
   imports: [
     PostsModule,
-    UsersModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
