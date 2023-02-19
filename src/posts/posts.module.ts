1c660cb7b2aa9325fba92f9c624edb75de6dd0f9
Mbonu Basil <mbonubasil@gmail.com>
2023-02-19 11:35:34 +0100

    Add Auth to forFeature imports so as to access authRepository from PostService

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -3,9 +3,10 @@ import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from './entities/post.entity';
+import { Auth } from 'src/auth/entities/auth.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Post])],
+  imports: [TypeOrmModule.forFeature([Post, Auth])],
   controllers: [PostsController],
   providers: [PostsService]
 })
